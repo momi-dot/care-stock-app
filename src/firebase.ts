b2c618e9db1fd7be791ba2628c00140f48e50026
momi-dot <momi@qccg.jp>
2026-04-03 20:11:35 +0900

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "ここに貼る",
-  authDomain: "ここに貼る",
-  projectId: "ここに貼る",
-  storageBucket: "ここに貼る",
-  messagingSenderId: "ここに貼る",
-  appId: "ここに貼る"
+  apiKey: "←貼る",
+  authDomain: "←貼る",
+  projectId: "care-stock-app",
+  storageBucket: "←貼る",
+  messagingSenderId: "←貼る",
+  appId: "←貼る"
 };
 
 const app = initializeApp(firebaseConfig);
